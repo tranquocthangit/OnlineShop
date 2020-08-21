1ab694752a593b53569c0630fb6290cb8ab81727
DESKTOP-BGVA95A\tranq <tranquocthang.it@gmail.com>
2020-08-21 13:58:03 +0700

    the first commit

---
+++ OnlineShop.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using OnlineShop.Data.Infrastructure;
+using OnlineShop.Model.Models;
+
+namespace OnlineShop.Data.Repositories
+{
+    public interface IProductRepository
+    {
+    }
+
+    internal class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ OnlineShop.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OnlineShop.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OnlineShop.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51c981e3-ec99-48c7-93c8-e67be6143268")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
